37896719477ba14809753bbd37825dde613a5521
Doruk Efe Gökmen <dgoekmen@ethz.ch>
2022-02-15 15:59:50 +0100

    added classifiers in setup.py file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='rsmine',
-    version='0.1.1',
+    version='0.1.1rc1',
     packages=find_packages(include=['rsmine', 'rsmine.*']),
     author='Doruk Efe Gökmen',
     author_email='dgoekmen@ethz.ch',
@@ -15,7 +15,8 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/RSMI-NE/RSMI-NE',
     project_urls={
-        "Bug Tracker": "https://github.com/RSMI-NE/RSMI-NE/issues"
+        'Source': 'https://github.com/RSMI-NE/RSMI-NE',
+        'Bug Tracker': 'https://github.com/RSMI-NE/RSMI-NE/issues'
     },
     license='Apache License 2.0',
     install_requires=['requests',
@@ -28,4 +29,20 @@ setup(
                       'tqdm',
                       'networkx',
                       'scipy'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='renormalization',
 )
